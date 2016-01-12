d5a4a2413e87f5e84dd3aa447df7a42cf3053d0f
Colin Woodbury <colingw@gmail.com>
2016-01-11 16:59:46 -0800

    Update

--- rna.hs
+++ rna.hs
@@ -6,17 +6,17 @@ import Shelly
 
 readAndEx :: FilePath -> Sh ()
 readAndEx f = do
+  let f' = toTextIgnore f
   newText <- ("-x " <>) <$> readfile f
   writefile "parsadata_erd_args" newText
-  run_ "PARSA_ERD_linux_2_6" []
-  let newDir = "RNApic/pic_" <> f
+  output <- run "PARSA_ERD_linux_2_6" []
+  let newDir = fromText $ "RNApic/pic_" <> f'
   mkdir_p newDir
   ls "pic" >>= mapM_ (\file -> cp file newDir)
+  writefile (newDir <> (fromText $ "parsa_output_" <> f')) output
 
 work :: Sh ()
-work = do
-  files <- ((<> "RNAfiles") <$> pwd) >>= ls
-  mapM_ readAndEx files
+work = ((<> "RNAfiles") <$> pwd) >>= ls >>= mapM_ readAndEx
 
 main :: IO ()
 main = shelly work
